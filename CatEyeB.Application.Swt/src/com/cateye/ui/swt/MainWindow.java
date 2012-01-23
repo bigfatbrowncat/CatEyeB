529157ab388e271e9249f6d76065098e69dffa12
Ilya Mizus <bigfatbrowncat@gmail.com>
2012-01-23 17:33:22 +0400

    Added preciseImageViewer with simple cells layout.

--- CatEyeB.Application.Swt/src/com/cateye/ui/swt/MainWindow.java
+++ CatEyeB.Application.Swt/src/com/cateye/ui/swt/MainWindow.java
@@ -1,6 +1,8 @@
 package com.cateye.ui.swt;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.internal.ImageList;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -54,10 +56,22 @@ public class MainWindow
 		fillLayout.type = SWT.VERTICAL;
 		shell.setLayout(fillLayout);
 		
-		final PreviewImageViewer imageViewer = new PreviewImageViewer(shell);
+		final PreciseImageViewer imageViewer = new PreciseImageViewer(shell);
 		imageViewer.setSize(400, 250);
 		
 		Image image = imageLoader.loadImageFromFile("..//..//data//test//IMG_5697.CR2");
-		imageViewer.setBitmap(image.getDescription().getThumbnail());
+		//imageViewer.setBitmap(image.getDescription().getThumbnail());
+		PreciseBitmapsVernissage vern = new PreciseBitmapsVernissage(2, 2);
+		for (int j = 0; j < 2; j++)
+		for (int i = 0; i < 2; i++)
+		{
+			vern.setUpdated(i, j, true);
+			vern.setBitmap(i, j, image.getBitmap());
+		}
+		vern.setUpdated(0, 0, false);
+		vern.setCaption(1, 0, "This is the caption for (1, 0) picture"); 
+		
+		imageViewer.setVernissage(vern);
+		//imageViewer.setBitmap(image.getBitmap());
 	}
 }
