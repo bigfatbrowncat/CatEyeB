34366f574e61cec0e0171de1a4222e267d5eccd4
Ilya Mizus <bigfatbrowncat@gmail.com>
2012-03-22 17:19:18 +0400

    Skin blending works now

--- CatEyeB.UI.SwtImageViewer/src/com/cateye/ui/swt/Scale.java
+++ CatEyeB.UI.SwtImageViewer/src/com/cateye/ui/swt/Scale.java
@@ -3,6 +3,8 @@ package com.cateye.ui.swt;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Canvas;
@@ -10,33 +12,62 @@ import org.eclipse.swt.widgets.Composite;
 
 public class Scale extends Canvas
 {
+	private Image skin;
+	
+	private void makeSkin(Device device, String filename, int foreR, int foreG, int foreB, int backR, int backG, int backB)
+	{
+		Image img = new Image(device, filename);
+		
+		ImageData textureMaskData = img.getImageData();
+		ImageData resData = new ImageData(textureMaskData.width, textureMaskData.height, textureMaskData.depth, textureMaskData.palette);
+		
+		resData.alphaData = new byte[resData.width * resData.height];
+		
+		System.out.println((byte)132);
+		for (int i = 0; i < textureMaskData.width * textureMaskData.height; i++)
+		{
+			int red_mask = (textureMaskData.data[i * textureMaskData.depth / 8 + 2] + 256) % 256;				// frame bump
+			int green_mask = (textureMaskData.data[i * textureMaskData.depth / 8 + 1] + 256) % 256;		// inner picture/color
+			int blue_mask = (textureMaskData.data[i * textureMaskData.depth / 8] + 256) % 256;
+			
+			int r = Math.min((foreR * blue_mask + backR * red_mask) / (blue_mask + red_mask + 1), green_mask);
+			int g = Math.min((foreG * blue_mask + backG * red_mask) / (blue_mask + red_mask + 1), green_mask);
+			int b = Math.min((foreB * blue_mask + backB * red_mask) / (blue_mask + red_mask + 1), green_mask);
+
+			resData.alphaData[i] = (byte)(Math.max(Math.max(255 - green_mask, red_mask), blue_mask));
+			resData.data[i * textureMaskData.depth / 8 + 2] = (byte)r;
+			resData.data[i * textureMaskData.depth / 8 + 1] = (byte)g;
+			resData.data[i * textureMaskData.depth / 8 + 0] = (byte)b;
+		}
+	
+		skin = new Image(device, resData);
+				
+	}
+	
 	public Scale(Composite parent)
 	{
 		super(parent, SWT.NO_BACKGROUND);
+
+		Color foreColor = getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND); 
 		
+		int fore_red = foreColor.getRed();
+		int fore_green = foreColor.getGreen();
+		int fore_blue = foreColor.getBlue();
+
+		int back_red = (foreColor.getRed() + 255) / 2;
+		int back_green = (foreColor.getGreen() + 255) / 2;
+		int back_blue = (foreColor.getBlue() + 255) / 2;
+		
+		System.out.println(fore_red);
+		
+		makeSkin(getDisplay(), "../../res/scale_texturemask.png", fore_red, fore_green, fore_blue, back_red, back_green, back_blue);
 		addPaintListener(new PaintListener()
 		{
 			@Override
 			public void paintControl(PaintEvent e)
 			{
-				org.eclipse.swt.graphics.Image img = new Image(e.display, "../../scale.png");
-				org.eclipse.swt.graphics.Image img_alpha = new Image(e.display, "../../scale_mask.png");
-				
-				ImageData idata = img.getImageData();
-				ImageData mask_id = img_alpha.getImageData();
-				
-				idata.alphaData = new byte[idata.width * idata.height];
-				
-				for (int i = 0; i < idata.alphaData.length; i++)
-				{
-					byte mask = (byte)(255 - mask_id.data[i * mask_id.depth / 8]);
-					
-					idata.alphaData[i] = (byte) mask;
-				}
-			
-				img = new Image(e.display, idata);
-				
-				e.gc.drawImage(img, 0, 0);
+
+				e.gc.drawImage(skin, -30, 0);
 				//draw(e.gc.handle);
 			}
 		});
